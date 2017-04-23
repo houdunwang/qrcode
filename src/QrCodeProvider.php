41661021c02d58cce34ec0d2fdf42f7d882d50b0
houdunwang <2300071698@qq.com>
2017-04-23 19:46:50 +0800

    no message

--- src/QrCodeProvider.php
+++ src/QrCodeProvider.php
@@ -7,17 +7,23 @@
  * |    WeChat: aihoudun
  * | Copyright (c) 2012-2019, www.houdunwang.com. All Rights Reserved.
  * '-------------------------------------------------------------------*/
+
 namespace houdunwang\qrcode;
 
 use houdunwang\framework\build\Provider;
 
-class QrCodeProvider extends Provider {
-	//延迟加载
-	public $defer = true;
+class QrCodeProvider extends Provider
+{
+    //延迟加载
+    public $defer = true;
 
-	public function register() {
-		$this->app->single( 'QrCode', function () {
-			return QrCode::single();
-		} );
-	}
+    public function register()
+    {
+        $this->app->single(
+            'QrCode',
+            function () {
+                return QrCode::single();
+            }
+        );
+    }
 }
