c33ea1e520f03deb0da75d61fe5cce09bcd1df2a
houdunwang <2300071698@qq.com>
2017-05-04 16:16:42 +0800

    no message

--- demo.php
+++
@@ -1,8 +0,0 @@
-<?php
-require 'vendor/autoload.php';
-\houdunwang\qrcode\QrCode::width( 500 )
-                         ->height( 500 )
-                         ->backColor( 255, 255, 255 )
-                         ->foreColor( 0, 0, 0 )
-                         ->save( 'http://houdunren.com','qr.png' );
-
